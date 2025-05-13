6283682fc4030d8f35516125d16c1a7b445c1dc5
Shimizu Yutaro <simiyu@shift-crops.net>
2025-05-13 12:37:01 +0900

    [git/lazygit] mkdir .local/bin, [git/delta] install curl

--- git/setup.sh
+++ git/setup.sh
@@ -26,14 +26,18 @@ if [ ! $(which lazygit) ]; then
     (cd $XDG_DATA_LG && \
     git apply pin.patch && \
     go build)
-    ln -is "$XDG_DATA_LG/lazygit" $HOME/.local/bin
+    [[ ! -d "$HOME/.local/bin" ]] && mkdir -p "$HOME/.local/bin"
+    ln -is "$XDG_DATA_LG/lazygit" "$HOME/.local/bin/lazygit"
 fi
 
 [[ ! -s "$XDG_CONFIG_LG/config.yml" ]] && ln -is "$PWD/lazygit/config.yml" "$XDG_CONFIG_LG/config.yml"
 
 if [ ! $(which delta) ]; then
+    if [ ! $(which curl) ]; then
+        sudo apt install curl
+    fi
     DELTA_VERSION=$(curl -s "https://api.github.com/repos/dandavison/delta/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
     curl -L "https://github.com/dandavison/delta/releases/download/${DELTA_VERSION}/git-delta_${DELTA_VERSION}_amd64.deb" -o /tmp/git-delta.deb
-    sudo dpkg -i /tmp/git-delta.deb
+    sudo dpkg -i /tmp/git-delta.deb && rm /tmp/git-delta.deb
 fi
 
